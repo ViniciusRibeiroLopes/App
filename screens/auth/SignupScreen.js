953463a39efe5453d45b9e81830bdceaf3c608dc
Enzo Gabriel Camillo <enzocamillo456@gmail.com>
2025-05-24 14:59:26 -0300

    Adicionando o firebase.

--- screens/auth/SignupScreen.js
+++ screens/auth/SignupScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 const RegisterScreen = () => {
 
@@ -13,26 +14,44 @@ const RegisterScreen = () => {
     });
   };
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function signUp(){
+    auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+      console.log('user: ', userCredential);
+    }).catch(error =>{
+      if(error.code === 'auth/email-already-in-use'){
+        console.log('Este email já possui uma conta!');
+      }else if(error.code === 'auth/invalid-email'){
+        console.log('Este email não existe!');
+      } else {
+        console.error('Erro inesperado ao criar conta:', error);
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Criar Conta</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Nome"
-      />
       <TextInput
         style={styles.input}
         placeholder="Email"
         keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
         style={styles.input}
         placeholder="Senha"
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={signUp}>
         <Text style={styles.buttonText}>Registrar</Text>
       </TouchableOpacity>
 
